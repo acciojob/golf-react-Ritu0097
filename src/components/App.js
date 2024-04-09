c0ab8246fa7fe11e04b6db4f6bf3029bea858ffc
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-09 14:12:35 +0530

    ball

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,42 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             renderBall: false,
-            posi : 0,
+            posi: 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
     }
 
-    // bind ArrowRight keydown event
+    buttonClickHandler() {
+        this.setState({ renderBall: true });
+    }
+
     componentDidMount() {
-      
+        document.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown = (event) => {
+        if (event.key === 'ArrowRight' && this.state.renderBall) {
+            this.setState(prevState => ({
+                ballPosition: { left: (parseInt(prevState.ballPosition.left) + 5) + "px" }
+            }));
+        }
+    }
+
+    renderBallOrButton() {
+        if (this.state.renderBall) {
+            return <div className="ball" style={this.state.ballPosition}></div>
+        } else {
+            return <button onClick={() => this.buttonClickHandler()}>Start</button>
+        }
     }
 
     render() {
@@ -37,6 +47,5 @@ class App extends Component {
         )
     }
 }
-
 
 export default App;
